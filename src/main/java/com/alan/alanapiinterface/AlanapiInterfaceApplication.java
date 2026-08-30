6579fdecb18e10357f460f137c3ffca8a1c20efd
yangsaichao <alanysc@qq.com>
2026-08-30 15:36:29 +0800

    Initial commit

---
+++ src/main/java/com/alan/alanapiinterface/AlanapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.alan.alanapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlanapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlanapiInterfaceApplication.class, args);
+    }
+
+}
